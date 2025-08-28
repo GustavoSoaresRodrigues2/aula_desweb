f9d44283601547fef31cdf3ff9b667370cb2aeac
Gustavo Soares Rodrigues <825231371@ulife.com.br>
2025-08-28 20:51:46 -0300

    primeira aula react / fazendo interface

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div style={{margin: 0, padding: 0, backgroundColor: "#EEE", borderRadius: 15}}>
+      <label htmlFor="nome" style={{}}>
+        Nome:
+      </label>
+      <input type="text" />
+    </div>
+  )
+}
+
+export default App
