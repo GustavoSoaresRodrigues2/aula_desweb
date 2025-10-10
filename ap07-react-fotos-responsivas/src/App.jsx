481a9c4ca8ffcfd863d7c67135f6b59ffd6f3b4c
Gustavo Soares Rodrigues <825231371@ulife.com.br>
2025-10-09 21:50:21 -0300

    fazendo a API Pexels, falta trazer a img

--- ap07-react-fotos-responsivas/src/App.jsx
+++ ap07-react-fotos-responsivas/src/App.jsx
@@ -1,16 +1,42 @@
+import React from 'react'
 import './App.css'
 import Busca from './components/Busca'
+import {createClient} from 'pexels'
 
-function App() {
-  return (
-    <>
-      <Busca />
-      <section className='flex mb-3'>
-        <i className='pi pi-car mr-3'></i>
-        <i className='pi pi-car mr-3'></i>
-      </section>
-    </>
-  )
+class App extends React.Component {
+  chavePexels = 'iUK2I4jiz2UXpWR0mK7E8AF9GVzlkGLA3RPMtZo57qN3nkvr9OwWWGR0'
+  pexelsClient = null
+  componentDidMount() {
+    this.pexelsClient = createClient(this.chavePexels)
+  }
+
+  onBuscaRealizada = (inputText) => {
+    this.pexelsClient.photos.search({
+      query: inputText
+    })
+    .then((result) => console.log(result))
+  }
+
+  render() {
+    return (
+      <>
+        <section className='grid border-1 justify-content-center border-400 border-round'>
+          <section className='col-12'>
+            <section className='flex justify-content-center mb-3'>
+              <i className='pi pi-car mr-3'></i>
+              <i className='pi pi-car mr-3'></i>
+            </section>
+          </section>
+          <section className='col-12 flex justify-content-center'>
+            <h1>Exibir uma lista de...</h1>
+          </section>
+          <section className='col-8'>
+            <Busca onBuscaRealizada={this.onBuscaRealizada} />
+          </section>
+        </section>
+      </>
+    )
+  }
 }
 
 export default App
--- ap07-react-fotos-responsivas/src/components/Busca.jsx
+++ ap07-react-fotos-responsivas/src/components/Busca.jsx
@@ -1,25 +1,47 @@
-import React, { Component } from 'react'
-import {Button} from 'primereact/button'
-import {InputText} from 'primereact/inputtext'
-import {InputIcon} from 'primereact/inputicon'
-import {IconField} from 'primereact/iconfield'
+import React, { useState, Component } from 'react'
+import { Button } from 'primereact/button'
+import { InputText } from 'primereact/inputtext'
+import { InputIcon } from 'primereact/inputicon'
+import { IconField } from 'primereact/iconfield'
 
 export default class Busca extends Component {
     state = {
         termoDeBusca: ''
     }
 
+    onTermoAlterado = (event) => {
+        this.setState({ termoDeBusca: event.target.value })
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault()
+        this.props.onBuscaRealizada(this.state.termoDeBusca)
+    }
+
     render() {
         return (
             <>
-                <section className='flex flex-column'>
-                    <IconField className='w-full' iconPosition='left'>
-                        <InputIcon className='pi pi-search' />
-                        <InputText className='w-full' placeholder='Buscar...' />
-                    </IconField>
-                    <Button className='mt-5' label='Procurar' />
-                </section>
+                <form action="" onSubmit={this.onFormSubmit}>
+                    <section className='flex flex-column'>
+                        <IconField className='w-full' iconPosition='left'>
+                            <InputIcon className='pi pi-search' />
+                            <InputText className='w-full' 
+                                placeholder={this.props.dica}
+                                onChange={this.onTermoAlterado} 
+                                value={this.state.termoDeBusca}
+                            />
+                        </IconField>
+                        <Button className='mt-5' label='Aparecer Imagens'
+                            onClick={this.onTermoAlterado}
+                        />
+                    </section>
+                </form>
+
             </>
         )
     }
+}
+
+Busca.defaultProps = {
+    dica: 'Buscar...'
 }
